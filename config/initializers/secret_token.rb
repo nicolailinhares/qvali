c117bb2c86dd8c9c45e4b86dc479f43b3102faa7
Nicolai Linhares <nicolai.linhares@gmail.com>
2013-01-15 09:38:20 -0200

    Inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qvali::Application.config.secret_token = 'd184de0895157e36242334850e3a6a4de491c36ef44a030e16d47abe6e77eb11ed37450563836588579514420eff071b5256de969c5470d95615b26cde385322'
